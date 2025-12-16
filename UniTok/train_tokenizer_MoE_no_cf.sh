f9d24e50fdf39c869a7add37525146e06b6f7f2f
Houyu <1808294049@qq.com>
2025-12-16 15:42:36 +0900

    Delete UniTok/train_tokenizer_MoE_no_cf.sh

--- UniTok/train_tokenizer_MoE_no_cf.sh
+++
@@ -1,7 +0,0 @@
-python ./MoE_RQ-VAE/main.py \
-  --device cuda:1 \
-  --data_path /home/user/yuhou/Item_tokenization/data_process/concat10.emb-llama-td.npy\
-  --alpha 0.01 \
-  --beta 0.0001 \
-  --cf_emb ./RQ-VAE/ckpt/Instruments-32d-sasrec.pt\
-  --ckpt_dir ../checkpoint/
